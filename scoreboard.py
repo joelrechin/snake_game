3f3b6bbbd16388f525c6ccfd9f8727882e3b5526
joel_rechin <83440135+joelrechin@users.noreply.github.com>
2022-04-03 15:04:40 -0700

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,42 @@
+from turtle import Turtle
+
+SCREEN_POSITION = [(-40,260),(40,260)]
+ALIGNMENT = "center"
+SIZE = 20
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.goto(SCREEN_POSITION[0])
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=("Arial", SIZE, "normal"))
+
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
+    def increase_point(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+
+
+
+
+
+
